8eb91b9dc147bef4d1d13dd22f3420481fa20a59
AnkanSaha18 <ankansaha314159@gmail.com>
2022-11-19 01:22:11 +0600

    Registration  done completely

--- application/forms.py
+++ application/forms.py
@@ -1,14 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo, Email
+from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
+from application.models import User
 
 
 class RegisterForm(FlaskForm):
+
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user is not None:
+            raise ValidationError('Username already exists! Please try a different username')
+
+    def validate_email(self, email_to_check):
+        email = User.query.filter_by(email=email_to_check.data).first()
+        if email is not None:
+            raise ValidationError('Email already exists! Please try a different email')
+
     username = StringField(label='Username', validators=[Length(min=5, max=50), DataRequired()])
     email = StringField(label='Email', validators=[Email(), DataRequired()])
     password1 = PasswordField(label='Password', validators=[Length(min=6), DataRequired()])
     password2 = PasswordField(label='Confirm Password', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Register')
-
-
-
