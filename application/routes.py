98dbcfbf4b027a8e1f5e0002c4944d4071c22539
AnkanSaha18 <ankansaha314159@gmail.com>
2022-11-23 03:08:51 +0600

    Summarization with link(problem in soup)

--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,9 @@
 from application import app
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from application.models import User
 from application.forms import RegisterForm
 from application import db
+from application import spacy_summary
 
 
 @app.route('/')
@@ -19,10 +20,7 @@ def news_page(news_type):
 @app.route('/login-registration', methods=['GET', 'POST'])
 def login_registration_page():
     register_form = RegisterForm()
-    print("this is before if")
-    print(register_form)
     if register_form.validate_on_submit():
-        print("this is after if")
         user_to_create = User(username=register_form.username.data,
                               email=register_form.email.data,
                               password_hash=register_form.password1.data)
@@ -36,3 +34,22 @@ def login_registration_page():
         return redirect(url_for('home_page'))
 
     return render_template('login_registration.html', form=register_form)
+
+
+@app.route('/summarize_link', methods=['GET', 'POST'])
+def summarize_link():
+    if request.method == 'POST':
+        link = request.form['link']
+        print("The link: {}".format(link))
+        headline, input_text = spacy_summary.get_headline_text(link)
+        input_text_reading_time = spacy_summary.calculate_reading_time(input_text)
+
+        summarized_text = spacy_summary.spacy_summary(headline, input_text_reading_time)
+        summarized_text_reading_time = spacy_summary.calculate_reading_time(summarized_text)
+
+        return render_template("summarize_link.html",
+                               input_text_reading_time=input_text_reading_time,
+                               summarized_text_reading_time=summarized_text_reading_time,
+                               input_text=input_text,
+                               summarized_text=summarized_text)
+    return render_template("summarize_link.html")
---
+++ application/spacy_summary.py
@@ -0,0 +1,86 @@
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+from string import punctuation
+from bs4 import BeautifulSoup as soup
+import requests
+
+nlp = spacy.load('en_core_web_sm')
+punctuation += '\n'
+
+
+def calculate_reading_time(text):
+    doc = nlp(text)
+    tokens = [token.text for token in doc]
+    total_words = len(tokens)
+    reading_time = total_words / 110.0
+    return reading_time
+
+
+def get_headline_text(url):
+    html = requests.get(url)
+    bsobj = soup(html.content, 'lxml')
+    headline = bsobj.title.text
+    text = ' '.join(map(lambda p: p.text, bsobj.findAll('p')))
+    return headline, text
+
+
+def spacy_summary(headline, text):
+    stopwords = list(STOP_WORDS)
+    doc = nlp(text)
+    head = nlp(headline)
+    tokens = [token.text for token in doc]
+    head_tokens = [htoken.text for htoken in head]
+    head_frequencies = {}
+    word_frequencies = {}
+    for word in head:
+        if word.text.lower() not in stopwords:
+            if word.text.lower() not in punctuation:
+                if word.text.lower() not in word_frequencies.keys():
+                    if word.text not in word_frequencies.keys():
+                        head_frequencies[word.text] = 1
+                    else:
+                        head_frequencies[word.text] += 1
+    for word in doc:
+        if word.text.lower() not in stopwords:
+            if word.text.lower() not in punctuation:
+                if word.text.lower() not in word_frequencies.keys():
+                    if word.text in head_frequencies.keys():
+                        if word.text not in word_frequencies.keys():
+                            word_frequencies[word.text] = 10
+                        else:
+                            word_frequencies[word.text] += 10
+                    else:
+                        if word.text not in word_frequencies.keys():
+                            word_frequencies[word.text] = 1
+                        else:
+                            word_frequencies[word.text] += 1
+    max_frequency = max(word_frequencies.values())
+    for word in word_frequencies.keys():
+        word_frequencies[word] = word_frequencies[word] / max_frequency
+    sentence_tokens = [sent for sent in doc.sents]
+    sentence_score = {}
+    for sent in sentence_tokens:
+        for word in sent:
+            if word.text.lower() in word_frequencies.keys():
+                if sent not in sentence_score.keys():
+                    sentence_score[sent] = word_frequencies[word.text.lower()]
+                else:
+                    sentence_score[sent] += word_frequencies[word.text.lower()]
+    sentence_scores_list = []
+    for sent in sentence_score:
+        sentence_scores_list.append(sentence_score[sent])
+    # sentence_scores_list
+    sentence_scores_list.sort(reverse=True)
+    sentence_scores_list
+    desired_summarized_text_persentage = 0.30
+    select_length = int(len(sentence_tokens) * desired_summarized_text_persentage)
+    del sentence_scores_list[select_length:]
+    summary = []
+    for sent in sentence_score:
+        if sentence_score[sent] in sentence_scores_list:
+            summary.append(sent)
+            sentence_scores_list.remove(sentence_score[sent])
+    final_summary = ''
+    for sent in summary:
+        final_summary += sent.text + ' '
+    return final_summary
